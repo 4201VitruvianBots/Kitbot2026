8b6c82c3ab0a097b3b67da3d3ce7d72d15621d48
Minecraftalus <frenzy961@gmail.com>
2026-01-13 20:23:37 -0800

    WIP flipping function for auto start pos

--- src/main/java/frc/robot/commands/autos/EightPieceMiddle.java
+++ src/main/java/frc/robot/commands/autos/EightPieceMiddle.java
@@ -7,7 +7,9 @@ package frc.robot.commands.autos;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.path.PathPlannerPath;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -21,13 +23,12 @@ public class EightPieceMiddle extends SequentialCommandGroup {
     try {
 
       PathPlannerPath path = PathPlannerPath.fromPathFile("EightPieceMiddlePath1");
-      var starting_pose = path.getStartingHolonomicPose().orElseThrow();
       var stopRequest = new SwerveRequest.ApplyRobotSpeeds();
 
       var m_path1 = swerveDrive.getTrajectoryUtils().generatePPHolonomicCommand("EightPieceMiddlePath1");
 
       addCommands(
-              new InstantCommand(() -> swerveDrive.resetPose(starting_pose)),
+              new InstantCommand(()->swerveDrive.resetPose(flippedStartingPosition(path))),
               m_path1.andThen(() -> swerveDrive.setControl(stopRequest)),
               new SetIntakeShooterSpeeds(intakeShooter, INTAKE_SPEED_PERCENT.INTAKE, INTAKE_SPEED_PERCENT.SHOOT),
               new WaitCommand(10)
@@ -36,5 +37,13 @@ public class EightPieceMiddle extends SequentialCommandGroup {
       DriverStation.reportError("Failed to load path for EightPieceMiddle", e.getStackTrace());
       addCommands(new InstantCommand());
     }
+  }
+  private static Pose2d flippedStartingPosition(PathPlannerPath path) {
+      boolean shouldFlip = DriverStation.getAlliance().orElse(Alliance.Blue).equals(DriverStation.Alliance.Red);
+      if(shouldFlip) {
+        return path.flipPath().getStartingHolonomicPose().get();
+      } else {
+        return path.getStartingHolonomicPose().get();
+      }
   }
 }
